cc8f3e2e851fc993df073b30bcb478918551e922
GruDev325 <seniordev1225@gmail.com>
2022-05-05 05:23:20 +0800

    add chart

---
+++ src/components/Barchart.vue
@@ -0,0 +1,72 @@
+<template>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, h, PropType } from 'vue'
+import { Line } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PluginOptionsByType } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default defineComponent({
+  name: 'LineChart',
+  components: { Line },
+  props: {
+    chartId: {
+      type: String,
+      default: 'line-chart'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object as PropType<Partial<CSSStyleDeclaration>>,
+      default: () => {}
+    },
+    plugins: {
+      type: Array as PropType<Plugin<'line'>[]>,
+      default: () => []
+    }
+  },
+  setup(props) {
+    const chartData = {
+      labels: [ 'January', 'February', 'March' ],
+      datasets: [ { data: [40, 20, 12] } ]
+    }
+
+    const chartOptions = { responsive: true }
+
+    return () =>
+      h(Bar, {
+        chartData,
+        chartOptions,
+        chartId: props.chartId,
+        width: props.width,
+        height: props.height,
+        cssClasses: props.cssClasses,
+        styles: props.styles,
+        plugins: props.plugins
+      })
+  }
+})
+</script>
